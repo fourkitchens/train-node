4b20c9fcf9988f1481fdf55f79799fca9fdd713d
Michal Minecki <michal@mirzu.com>
2013-06-21 00:31:46 -0500

    updated slides made the streams.js file more of a shell

--- streams/streams.js
+++ streams/streams.js
@@ -2,26 +2,37 @@
  * @fileoverview demonstrates streams.
  */
 
-// reading a file from the file system.
 /*
+// reading a file from the file system.
 var fs = require('fs');
 
 var file = fs.createReadStream('index.html');
 
-file.on('data', function(chunk){
-  console.log(chunk.toString());
-});
 */
+/*
+// Using the readFile function instead to read
+// the entire contents of the file.
+var fs = require('fs');
+
+*/
+
+/*
+// Create a server using readFile function.
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(function (req, res) {
+
+});
+server.listen(21000);
+*/
+
+/*
+// Create a server by piping the contents into the response.
 var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function (req, res) {
-    fs.readFile('index.html', function (err, data) {
-        if (err) {
-            res.statusCode = 500;
-            res.end(String(err));
-        }
-        else res.end(data);
-    });
+
 });
 server.listen(21000);
+*/
